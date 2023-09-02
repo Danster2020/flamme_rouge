0455d59276ccb0b84540d77be5f1d03b31be0e71
Danster2020 <daniel.bleckert@me.com>
2023-09-02 14:13:27 +0200

    removed test

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { GameFlammeRouge } from "./Game";
 
 const server = Server({
     games: [GameFlammeRouge],
-    origins: [Origins.LOCALHOST_IN_DEVELOPMENT, "192.168.1.5"],
+    origins: [Origins.LOCALHOST],
 });
 
 const server_port = 8001;
